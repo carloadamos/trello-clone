33e7168a7b68b6e9707531e8f5185c7e7c8ea245
carloadamos <johncarloadamos@gmail.com>
2020-10-03 22:45:11 +0800

    Card list react hook conversion

--- src/components/card-list/card-list.component.js
+++ src/components/card-list/card-list.component.js
@@ -1,5 +1,5 @@
 // Package
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Droppable } from "react-beautiful-dnd";
 
 // Component
@@ -19,74 +19,67 @@ const getListStyle = (isDraggingOver) => ({
   width: 250,
 });
 
-export default class CardList extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      addingItem: false,
-    };
-  }
-
-  render() {
-    const { index, taskList } = this.props;
-
-    return (
-      <Droppable droppableId={String(index)}>
-        {(provided, snapshot) => (
-          <div
-            className="card-list"
-            ref={provided.innerRef}
-            style={getListStyle(snapshot.isDraggingOver)}
-            {...provided.droppableProps}
-          >
-            <p>{taskList.title}</p>
-            {taskList.tasks.map((item, index) => (
-              <CardItem
-                key={convertToStringId(item)}
-                item={item}
-                index={index}
-              />
-            ))}
-            {provided.placeholder}
-
-            <div
-              className="card-list__add"
-              onClick={() => {
-                this.setState({ addingItem: true });
-              }}
-            >
-              {!this.state.addingItem ? (
-                <p>Add item</p>
-              ) : (
-                this._renderInputField()
-              )}
-            </div>
-          </div>
-        )}
-      </Droppable>
-    );
-  }
+const CardList = props => {
+  const [addingItem, setAddingItem] = useState(false);
+  const { index, taskList, addItem } = props;
 
   /**
    * Render textbox for adding new task.
    */
-  _renderInputField() {
-    return <input type="text" onKeyDown={this._handleKeyDown} />;
+  const _renderInputField = () => {
+    return <input type="text" onKeyDown={_handleKeyDown} />;
   }
 
   /**
    * Kydown handler.
    * @param {Object} event Event object.
    */
-  _handleKeyDown = (event) => {
+  const _handleKeyDown = (event) => {
     if (event.key === "Enter") {
-      this.props.addItem(this.props.index, event.target.value);
-      this.setState({ addingItem: false });
+      addItem(index, event.target.value);
+      setAddingItem(false);
     }
 
     if (event.key === "Escape") {
-      this.setState({ addingItem: false });
+      setAddingItem(false);
     }
   };
+
+  return (
+    <Droppable droppableId={String(index)}>
+      {(provided, snapshot) => (
+        <div
+          className="card-list"
+          ref={provided.innerRef}
+          style={getListStyle(snapshot.isDraggingOver)}
+          {...provided.droppableProps}
+        >
+          <p>{taskList.title}</p>
+          {taskList.tasks.map((item, index) => (
+            <CardItem
+              key={convertToStringId(item)}
+              item={item}
+              index={index}
+            />
+          ))}
+          {provided.placeholder}
+
+          <div
+            className="card-list__add"
+            onClick={() => {
+              setAddingItem(true);
+            }}
+          >
+            {!addingItem ? (
+              <p>Add item</p>
+            ) : (
+                _renderInputField()
+              )}
+          </div>
+        </div>
+      )}
+    </Droppable>
+  );
 }
+
+export default CardList;
